7fef32a8895c8ff383defc29dcba05a0b21b102f
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-11-28 19:15:43 +0100

    Move heartbeat routes to root path. And set fixed sensorthings version

--- test/test_heartbeat.js
+++ test/test_heartbeat.js
@@ -6,14 +6,12 @@ import config from '../src/config';
 
 const server = supertest.agent(app);
 
-const endpointPrefix = '/' + config.get('version');
-
 describe('Health checks', () => {
   ['/',
    '/__heartbeat__',
    '/__lheartbeat__'].forEach(endpoint => {
     it('should response 200 OK to GET ' + endpoint + ' request', done => {
-      server.get(endpointPrefix + endpoint)
+      server.get(endpoint)
       .expect(200)
       .end((err, res) => {
         should.not.exist(err);
@@ -25,7 +23,7 @@ describe('Health checks', () => {
 
   it('should response 200 with JSON file to GET /__version__ request',
      done => {
-    server.get(endpointPrefix + '/__version__')
+    server.get('/__version__')
     .expect(200)
     .expect('Content-type', /json/)
     .end((err, res) => {
